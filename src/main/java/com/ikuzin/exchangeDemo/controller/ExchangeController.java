468cc787ab5c862add6ef63fa7744408d1d9f410
ivan <jobkuzin@yandex.ru>
2021-07-26 13:07:47 +0300

    remove controller constructor

--- src/main/java/com/ikuzin/exchangeDemo/controller/ExchangeController.java
+++ src/main/java/com/ikuzin/exchangeDemo/controller/ExchangeController.java
@@ -2,7 +2,7 @@ package com.ikuzin.exchangeDemo.controller;
 
 import com.ikuzin.exchangeDemo.service.ExchangeService;
 import com.ikuzin.exchangeDemo.utils.Utils;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,13 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 public class ExchangeController {
-
     private final ExchangeService exchangeService;
-
-    public ExchangeController(@Autowired ExchangeService exchangeService) {
-        this.exchangeService = exchangeService;
-    }
 
     @GetMapping(value = "/valuta/{valutaCode}", produces = MediaType.IMAGE_GIF_VALUE)
     public byte[] getValutaTrend(@PathVariable String valutaCode) {
